87325858a21f13b377403023b73d8969dceee179
Omar A <iberamen@gmail.com>
2015-01-13 18:40:34 +0000

    Working with Arrays

--- Module06/mod06/AnnualClimatev1.java
+++ Module06/mod06/AnnualClimatev1.java
@@ -4,8 +4,21 @@ class AnnualClimatev1 {
     
     public static void main(String[] args) {
         
+        //Decalre the scanner
+        Scanner in = new Scanner(System.in);
+        
+        //Declare the variables
+        String f = "F";
+        String c = "C";
+        String inches = "Inches";
+        String cm = "cm";
+        String city = "Miami";
+        String state = "Florida";
         
         
+        System.out.println("Climate Data");
+        System.out.println("Location: " + city + ", " + state);
+        System.out.println("Month\t" + "Temperature (F)\t" + "Precipitation (inches)");
     }
     
 }
