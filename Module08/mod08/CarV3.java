de83596670ad62ebf69bf10b3a4def8c1c3e6a23
IBeRamen <iberamen@gmail.com>
2015-02-10 10:23:48 -0500

    Added PMR

--- Module08/mod08/CarV3.java
+++ Module08/mod08/CarV3.java
@@ -1,5 +1,23 @@
 package Module08.mod08;
 
+/**
+ * Project title: 08.03 Default constructors.
+ *
+ * Purpose of Project: Calculates the gas mileage on an example car.
+ *
+ * @version 2/10/2015
+ *
+ * @author Omar Rahman
+ *
+ * *********** PMR ***********
+ * (+) Understood OOP more.
+ *
+ * (-) Need more practice with OOP.
+ * ***************************
+ *
+ * In the future: Practice.
+ */
+
 public class CarV3 {
 
     private String carType;
