27841984340d5f764eb8a4ffcdeb149101b52d42
IBeRamen <iberamen@gmail.com>
2015-02-12 08:18:34 -0500

    Used a better method to find the points

--- Module07/mod07/PointsOnACircleV1.java
+++ Module07/mod07/PointsOnACircleV1.java
@@ -22,24 +22,19 @@ public class PointsOnACircleV1 {
 
     public static void main(String[] args) {
 
-        double[] numberList = {1.0, 0.90, 0.80, 0.70, 0.60, 0.50, 0.40, 0.30, 0.20, 0.10, 0.00, -0.10, -0.20, -0.30, -0.40, -0.50, -0.60, -0.70,-0.80, -0.90, -1.00};
+        //Input
         double radius = 1.0;
+        double x = 1.0;
+        double y = 1.0;
 
         //Output
-        System.out.println("     Points on a Circle of Radius 1.0");
-        System.out.println("     x1     y1    x2     y2");
-        System.out.println("---------------------------------------------");
-
-        for (int z = 0; z < numberList.length; z++) {
-            double y1 = Math.sqrt(Math.pow(radius, 2) - Math.pow(numberList[z], 2));
-            double y2 = 0.00;
-
-            if (y1 != 0.00)
-            {
-                y2 = (-y1);
-            }
-            System.out.printf("%10.2f%10.2f%10.2f%10.2f%n", numberList[z], y1, numberList[z], y2);
+        System.out.println("Points on a Circle of Radius " +radius);
+        System.out.println("x1     y1              x2       y2");
+        System.out.println("---------------------------------------");
+        for(int counter = 0; counter < 20; counter++){
+            x-= 0.1;
+            double solveY = Math.sqrt((Math.pow(radius, 2) - Math.pow(x, 2)));
+            System.out.printf("%5.2f     %5.2f            %5.2f     %5.2f\n" , x, solveY, x, -1 * solveY);
         }
-
     }
 }
