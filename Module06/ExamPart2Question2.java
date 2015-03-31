f61e8731efab8b6cac2e21d6651eb5fc59276112
IBeRamen <iberamen@gmail.com>
2015-03-31 09:51:23 -0400

    Finished ExamPart2Question2

---
+++ Module06/ExamPart2Question2.java
@@ -0,0 +1,30 @@
+package Module06;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by iberamen on 3/31/15.
+ */
+public class ExamPart2Question2 {
+
+    public static void main(String[] args) {
+
+        double[] a1 = {1.2, 2.3, 3.4, 4.5, 5.6};
+
+        double[] a2 = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+        double[] a3 = new double[5];
+
+
+        for (int i = 0; i < a1.length; i++) {
+            a3[i] = a1[i] + a2[i];
+        }
+        for (int i = 0; i < a1.length; i++) {
+            System.out.println(a3[i]);
+        }
+    }
+
+}
