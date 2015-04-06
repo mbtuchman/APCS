93eb9c1947c0ce46ebab489402cc23f1e178565b
Omar A <iberamen@gmail.com>
2015-04-06 18:22:18 -0400

    Finishing ExamPart2

--- Module08/ExamPart2Question3.java
+++ Module08/ExamPart2Question3.java
@@ -6,4 +6,19 @@ package Module08;
  */
 
 public class ExamPart2Question3 {
+
+    public static String convertName(String name) {
+
+        String firstName = name.substring(0, name.indexOf(" "));
+        String lastName = name.substring(name.indexOf(" "));
+        return String.format("%s, %s", lastName, firstName);
+
+    }
+
+    public static void main(String args[]) {
+        String name = "Grace Hopper";
+        name = convertName(name);
+        System.out.println(name);
+    }
+
 }
