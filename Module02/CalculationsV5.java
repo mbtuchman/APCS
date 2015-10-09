96d4bda69fecea26ee72303a2ac6a6964a012245
The Raccoon <fdizile@gmail.com>
2015-10-09 15:56:26 -0400

    Update CalculationsV5.java

--- Module02/CalculationsV5.java
+++ Module02/CalculationsV5.java
@@ -65,12 +65,6 @@ public class CalculationsV5
         System.out.println("(46.2 divided by 11) minus 3 plus 24 modulus (17 minus 2 times 3) equals " + ((46.2 / 11) - 3 + 24 % (17 - 2 * 3)));
         // 480 divided by 10 divided by 12 plus 200 *.5 minus 20 modulus 8
         System.out.println("480 divided by 10 divided by 12 plus 200 *.5 minus 20 modulus 8 equals " + (480 / 10 / 12 + 200 * 0.5 - 20 % 8));
-        
-        // 2.03 Additional int Equations
-        
-        
-        //2.04 Additional double Equations
-        
                
-    } // end of main method
-} // end of class
+    } // method main end
+} // class end
