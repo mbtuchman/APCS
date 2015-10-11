2c14c09a764bbe9908c6516caab84a0cbf633dc4
The Raccoon <fdizile@gmail.com>
2015-10-11 18:02:15 -0400

    Update CurrencyConversionV1.java

--- Module02/CurrencyConversionV1.java
+++ Module02/CurrencyConversionV1.java
@@ -59,34 +59,34 @@ public class CurrencyConversionV1
         // with the proper calculations. Casting, integer division and the modulus
         // operator needed. Do not worry about extra decimal places for the dollar amounts.
 
- 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("Souvenir Purchases");
-		System.out.println(" (all values in US Dollars) ");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+ 	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	System.out.println("Souvenir Purchases");
+	System.out.println(" (all values in US Dollars) ");
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-		//Calculations for Souvenir #1
-		int costItem1 = 4;  						//cost per item of first souvenir
-		int budget1 = 50;   						//budget for first item
-		int totalItem1 = budget1 / costItem1; 		//how many items can be purchased
-		double fundsRemaining1 = budget1 % totalItem1;  //how much of the budget is left
+	// Souvenir #1
+	int costItem1 = 4; // Cost (first item)
+	int budget1 = 50; // Budget (first item)
+	int totalItem1 = budget1 / costItem1; // Number of purchaseable items (first item)
+	double fundsRemaining1 = budget1 % totalItem1; // Budget left (first item)
 
-		System.out.println("Item 1");
-		System.out.println("   Cost per item: $" + costItem1);
-		System.out.println("   Budget: $"+ budget1);
-		System.out.println("   Total items purchased:" + " " + totalItem1);
-		System.out.println("   Funds remaining: $"  +  fundsRemaining1);
+	System.out.println("Item 1");
+	System.out.println("   Cost per item: $" + costItem1);
+	System.out.println("   Budget: $"+ budget1);
+	System.out.println("   Total items purchased:" + " " + totalItem1);
+	System.out.println("   Funds remaining: $"  +  fundsRemaining1);
 
-		//Calculations for Souvenir #2
-		double costItem2 = 32.55;  						//cost per item of second souvenir
-		int budget2 = 713;   							//budget for second item
-		int totalItem2 = (int) (budget2 / costItem2); 	//how many items can be purchased
-		double fundsRemaining2 = budget2 % totalItem2;  	//how much of the budget is left // I am not sure why the output is 33.
+	// Souvenir #2
+	double costItem2 = 32.55; // Cost (second item)
+	int budget2 = 713; // Budget (second item)
+	int totalItem2 = (int) (budget2 / costItem2); // Number of purchaseable items (second item)
+	double fundsRemaining2 = budget2 % totalItem2; // Budget left (second item)
 
-		System.out.println("Item 2");
-		System.out.println("   Cost per item: $" + costItem2);
-		System.out.println("   Budget: $"+ budget2);
-		System.out.println("   Total items purchased:" + " " + totalItem2);
-		System.out.println("   Funds remaining: $"  +  fundsRemaining2);
-
-    } // end of main method
-} // end of class
+	System.out.println("Item 2");
+	System.out.println("   Cost per item: $" + costItem2);
+	System.out.println("   Budget: $"+ budget2);
+	System.out.println("   Total items purchased:" + " " + totalItem2);
+	System.out.println("   Funds remaining: $"  +  fundsRemaining2);
+	
+    } // end main method
+} // end class
