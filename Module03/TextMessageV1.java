e6ac6ffb539691567836fbd10fab56218d5a1922
The Raccoon <fdizile@gmail.com>
2015-10-20 16:51:40 -0400

    Update TextMessageV1.java

--- Module03/TextMessageV1.java
+++ Module03/TextMessageV1.java
@@ -16,22 +16,22 @@ public class TextMessageV1 {
 		
 		// Output 1
 		String previousString = "why am i such a script kiddie";
-		System.out.println("String: " + previousString);
+		System.out.println("Old Text: " + previousString);
 		// String Length
 		int stringLength = previousString.length();
 		System.out.println("Length: " + stringLength);
 		// Replace Characters
 		String replaceCharacters = previousString.replace("script kiddie", "skid");
 		replaceCharacters = replaceCharacters.replace("why", "y");
-		System.out.println("Replace: " + replaceCharacters);
+		System.out.println("New Text: " + replaceCharacters);
 		// Split String
 		int meetMeHalfWay = stringLength / 2;
 		String firstPart = previousString.substring(0, meetMeHalfWay);
 		String secondPart = previousString.substring(meetMeHalfWay, stringLength);
 		String split = secondPart + "-" + firstPart; 
-		System.out.println("Split String: " + split);
+		System.out.println("Split String (Half): " + split);
 		// Sub String
-		System.out.print("Sub String: ");
+		System.out.print("Sub String (5, 8): ");
 		System.out.println(previousString.substring(5, 8));
 		// Remove Vowels
 		String noVowels = previousString.replaceAll("[aeiou]", "");
