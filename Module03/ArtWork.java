5c9b8213b55d934610c61db752f3525b1c64787f
fdizile <fdizile@gmail.com>
2015-10-21 12:33:32 -0400

    Updated ArtWork.java

--- Module03/ArtWork.java
+++ Module03/ArtWork.java
@@ -1,12 +1,12 @@
 /**
  * @author Max Tuchman (me@mbtuchman.com)
  * @description 03.04 ASCII Art
- * 
+ *
  * -=-=-=-= PMR =-=-=-=-
  * Pluses:
  * + I figured out how to make ASCII with the print method!
  * + ASCII is really fun!
- * 
+ *
  * Minuses:
  * - Formating was a pain in the butt.
  * - Printing the ASCII took a long-ish time. (I'll try to consolidate into a single string in future)
@@ -18,28 +18,51 @@ public class ArtWork
     public static void main(String [ ] args)
     {
         // Sets ASCII Strings (L1 = line 1, L2 = line 2, etc.)
-        String L1 = ".##.....##....###....##.....##";
-        String L2 = ".###...###...##.##....##...##.";
-        String L3 = ".####.####..##...##....##.##..";
-        String L4 = ".##.###.##.##.....##....###...";
-        String L5 = ".##.....##.#########...##.##..";
-        String L6 = ".##.....##.##.....##..##...##.";
-        String L7 = ".##.....##.##.....##.##.....##";
-        
+        // Max
+        String line1 = ".::       .::      .:       .::      .::";
+        String line2 = ".: .::   .:::     .: ::      .::   .::";
+        String line3 = ".:: .:: . .::    .:  .::      .:: .::";
+        String line4 = ".::  .::  .::   .::   .::       .::";
+        String line5 = ".::   .:  .::  .:::::: .::    .:: .::";
+        String line6 = ".::       .:: .::       .::  .::   .::";
+        String line7 = ".::       .::.::         .::.::      .::";
+        // Tuchman
+        String line8 = ".::: .::::::.::     .::    .::   .::     .::.::       .::      .:       .:::     .::";
+        String line9 = "     .::    .::     .:: .::   .::.::     .::.: .::   .:::     .: ::     .: .::   .::";
+        String line10 = "    .::    .::     .::.::       .::     .::.:: .:: . .::    .:  .::    .:: .::  .::";
+        String line11 = "    .::    .::     .::.::       .:::::: .::.::  .::  .::   .::   .::   .::  .:: .::";
+        String line12 = "    .::    .::     .::.::       .::     .::.::   .:  .::  .:::::: .::  .::   .: .::";
+        String line13 = "    .::    .::     .:: .::   .::.::     .::.::       .:: .::       .:: .::    .: ::";
+        String line14 = "    .::      .:::::      .::::  .::     .::.::       .::.::         .::.::      .::";
+
         // Prints actual ASCII
-        System.out.print(L1);
+        System.out.print(line1);
         System.out.print("\n");
-        System.out.print(L2);
+        System.out.print(line2);
         System.out.print("\n");
-        System.out.print(L3);
+        System.out.print(line3);
         System.out.print("\n");
-        System.out.print(L4);
+        System.out.print(line4);
         System.out.print("\n");
-        System.out.print(L5);
+        System.out.print(line5);
         System.out.print("\n");
-        System.out.print(L6);
+        System.out.print(line6);
         System.out.print("\n");
-        System.out.print(L7);
-        
+        System.out.print(line7);
+        System.out.print("\n\n");
+        System.out.print(line8);
+        System.out.print("\n");
+        System.out.print(line9);
+        System.out.print("\n");
+        System.out.print(line10);
+        System.out.print("\n");
+        System.out.print(line11);
+        System.out.print("\n");
+        System.out.print(line12);
+        System.out.print("\n");
+        System.out.print(line13);
+        System.out.print("\n");
+        System.out.print(line14);
+
     } // method end
 } // class end
