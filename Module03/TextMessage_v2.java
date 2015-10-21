d383b3e56d716c294ff8e7e8c360fe50eb164d61
fdizile <fdizile@gmail.com>
2015-10-21 12:47:28 -0400

    Updated Assignment Files

--- Module03/TextMessage_v2.java
+++ Module03/TextMessage_v2.java
@@ -1,34 +1,26 @@
-package Module03;
-
 import java.util.Scanner;
-
 /**
- * Project title: 03.07 Accepting User Input
- * 
- * Purpose of Project: Modifies strings.
- * 
- * @version 11/13/2014
- * 
- * @author Omar Rahman
- * 
- * *********** PMR ***********
- * (+) Learned how to add user input.
- * 
- * (-) Using commas makes the Split String come out with a "-" symbol, but it still works.
- * ***************************
- * 
- * In the future: Try more user input exercises.
+ * @description 03.07 Accepting User Input
+ * @author Max Tuchman (me@mbtuchman.com)
+ *
+ * -=-=-=-=-= PMR =-=-=-=-=-
+ * Pluses:
+ * + Added user Input
+ *
+ * Minuses:
+ * - Using commas in a split makes a "-" appear in output.
+ * -=-=-=-=-=-=-=-=-=-=-=-=-
  */
 
 public class TextMessageV2 {
-	
+
 	@SuppressWarnings("resource")
 	public static void main(String[ ] args) {
-		
+
 		// contruct the Scanner object.
 		Scanner in;
 		in = new Scanner(System.in);
-		
+
 		// Output 1
 		System.out.println("Please enter a sentence:  ");
 		String firstWord = in.next();
@@ -47,14 +39,14 @@ public class TextMessageV2 {
 		int meetMeHalfWay = stringLength / 2;
 		String firstPart = previousString.substring(0, meetMeHalfWay);
 		String secondPart = previousString.substring(meetMeHalfWay, stringLength);
-		String split = secondPart + "-" + firstPart; 
+		String split = secondPart + "-" + firstPart;
 		System.out.println("Split String: " + split);
 		// Remove Vowels
 		String noVowels = previousString.replaceAll("[aeiou]", "");
-		System.out.println("Team Consonants: " + noVowels);
+		System.out.println("All Consonants: " + noVowels);
 		// Remove Consonants
 		String noConsonants = previousString.replaceAll("[qwrtypsdfghjklzxcvbnm]", "");
-		System.out.println("Team Vowels: " + noConsonants);
+		System.out.println("All Vowels: " + noConsonants);
 	}
 
 }
